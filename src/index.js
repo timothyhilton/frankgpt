bc3b76b15815b67d6f7d0fb04c232fbe1b3b61a7
timothyhilton <recore444@gmail.com>
2024-03-02 16:40:46 +1000

    POC to send a message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require('discord.js');
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
-client.on("ready", () => {
-    console.log("Bot is ready")
-    client.user.setActivity('Ready to chat')
+client.on("ready", (client) => {
+    console.log(`${client.user.tag} is ready`);
+    client.user.setActivity('Ready to chat');
 })
 
-client.login('TOKEN');
+client.on("messageCreate", (message) => {
+    console.log(message.channelId);
+
+    message.channel.send('hi');
+});
+
+client.login(process.env.TOKEN);
